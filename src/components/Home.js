6504fdfcd4ed3810cf7c720e955f48defe96b3f9
SeanZhang-QED <66594541+SeanZhang-QED@users.noreply.github.com>
2021-11-23 03:32:01 -0800

    Adding the Tabs in the Home component.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,125 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Navigate} from "react-router-dom";
+import SearchBar from "./SearchBar";
+import {Backdrop, Box, CircularProgress, Container, Grid, Tab, Tabs} from "@mui/material";
+import {BASE_URL, SEARCH_KEY, TOKEN_KEY} from "../constants";
+import axios from "axios";
+import ImageIcon from '@mui/icons-material/Image';
+import MovieIcon from '@mui/icons-material/Movie';
+
 
 function Home(props) {
     const { isLoggedIn, handleAlert } = props;
+    const [ isLoading, setIsLoading ] = useState(false);
+    const [ posts, setPosts ] = useState([]);
+    const [ value, setValue] = useState(0);
+
+    const handleSearch = (data) => {
+        // get the data from search bar, then fetch data
+        const {type, keywords} = data;
+        // set loading
+        setIsLoading(true);
+        fetchPosts(data);
+    };
+
+    // didMount need to fetch the data from the backend for the first time
+    useEffect(()=>{
+        fetchPosts({
+            type:SEARCH_KEY.all,
+            keywords:'',
+        })
+    },[]);
+
+    // didUpdate, render the view when a set of new posts fetched
+    useEffect(()=>{
+        renderPosts(posts);
+    },[posts]);
+
+    const fetchPosts = (data) => {
+        const {type, keywords} = data;
+        // fetch data
+        // 1. url
+        let url = "";
+        if (type === SEARCH_KEY.all) {
+            url = `${BASE_URL}/search`;
+        } else if (type === SEARCH_KEY.user) {
+            url = `${BASE_URL}/search?user=${keywords}`;
+        } else {
+            url = `${BASE_URL}/search?keywords=${keywords}`;
+        }
+        // 2. opt
+        const opt = {
+            method: "GET",
+            url: url,
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem(TOKEN_KEY)}`
+            }
+        };
+        // 3. axios
+        axios(opt)
+            .then((res) => {
+                if (res.status === 200) {
+                    console.log(res.data);
+                    setPosts(res.data);
+                }
+            })
+            .catch((err) => {
+                handleAlert('error','Fetch posts failed!')
+                console.log("fetch posts failed: ", err.message);
+            })
+            .finally(()=>{
+                setIsLoading(false);
+            });
+    };
+
+    const renderPosts = (type) => {
+        if (type == 'image'){
+            return(
+                <div>
+                    Images.
+                </div>
+            )
+        }
+
+        return(
+            <div>
+                Videos.
+            </div>
+        )
+    }
+
     return (
         <div>
-            { !isLoggedIn ?
-                <Navigate to="/login" />
-                :
-                <>
-                    this is home
-                </>
-            }
+            <div>
+                <Backdrop
+                    sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                    open={isLoading}
+                >
+                    <CircularProgress color="inherit" />
+                </Backdrop>
+                { !isLoggedIn ?
+                    <Navigate to="/login" />
+                    :
+                    <SearchBar handleSearch={handleSearch}/>
+                }
+            </div>
+            <Container sx={{ py: 2, margin: 0}} >
+                <Grid container spacing={2}>
+                    <Grid item xs={1} sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 224 }}>
+                        <Tabs value={value}
+                              onChange={(event, newValue) => {setValue(newValue);console.log(newValue);}}
+                              orientation="vertical"
+                              sx={{ borderRight: 1, borderColor: 'divider' }}>
+                            <Tab icon={<ImageIcon />} label="Images" />
+                            <Tab icon={<MovieIcon />} label="Videos" />
+                        </Tabs>
+                    </Grid>
+                    <Grid item xs={10}>
+                        {value === 0 && renderPosts('image')}
+                        {value === 1 && renderPosts('video')}
+                    </Grid>
+                </Grid>
+            </Container>
         </div>
     );
 }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,84 @@
+import React, {useState} from 'react';
+import {
+    Box,
+    Divider,
+    FormControl,
+    FormControlLabel,
+    Grid,
+    IconButton, Input,
+    InputBase, Menu, MenuItem, MenuList,
+    Paper,
+    RadioGroup, Select, TextField, Typography
+} from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+import Radio from '@mui/material/Radio';
+import MenuIcon from '@mui/icons-material/Menu';
+import AllInclusiveIcon from '@mui/icons-material/AllInclusive';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ChatIcon from '@mui/icons-material/Chat';
+import {SEARCH_KEY} from "../constants";
+
+function SearchBar(props) {
+    const [ type, setType ] = useState(SEARCH_KEY.all);
+    const [ keyInfo, setKeyInfo ] = useState("");
+    const [error, setError] = useState("");
+
+
+
+    const handleSearch = () => {
+        // when to show the error message under the search bar
+        if (type !== SEARCH_KEY.all && keyInfo === "") {
+            setError("Please input your search keyword!");
+            return;
+        }
+        // need to remove error <- reset to ""
+        setError("");
+        let data = {
+            type: type,
+            keywords: keyInfo
+        }
+        props.handleSearch(data);
+    };
+
+
+    return (
+        <Box sx={{width: "500px", margin: "0 auto"}}>
+            <Paper component="form" sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 500 }}
+            >
+                <MenuIcon sx={{p:"10px"}}/>
+                <InputBase
+                    sx={{ ml: 1, flex: 1 }}
+                    type="text"
+                    placeholder="Search Gourmet Posts"
+                    onChange={event=>{setKeyInfo(event.target.value)}}
+                />
+                <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+                <IconButton
+                    sx={{ p: '10px' }}
+                    disabled={type == SEARCH_KEY.all}
+                    onClick={handleSearch}
+                >
+                    <SearchIcon />
+                </IconButton>
+            </Paper>
+            <Typography className="error-msg" textAlign="center" color="red">
+                { error }
+            </Typography>
+            <FormControl component="fieldset" sx={{ p: '2px 4px', width: 500 }}>
+                <RadioGroup row
+                            name="search-type-group"
+                            sx={{mt:1, display:'flex', justifyContent:'space-evenly'}}
+                            onChange={(e)=>{
+                                setType(e.target.value);
+                            }}
+                >
+                    <FormControlLabel value={SEARCH_KEY.all} control={<Radio />} label="All" />
+                    <FormControlLabel value={SEARCH_KEY.keyword} control={<Radio />} label="Keyword" />
+                    <FormControlLabel value={SEARCH_KEY.user} control={<Radio />} label="User" />
+                </RadioGroup>
+            </FormControl>
+        </Box>
+    );
+}
+
+export default SearchBar;
